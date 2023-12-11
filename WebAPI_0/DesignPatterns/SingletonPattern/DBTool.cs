d2bb7c470aa05cdc03fba07e1ea1d4252b06bb9b
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-12-11 14:00:13 +0300

    Add project files.

---
+++ WebAPI_0/DesignPatterns/SingletonPattern/DBTool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI_0.Models;
+
+namespace WebAPI_0.DesignPatterns.SingletonPattern
+{
+    public class DBTool
+    {
+        DBTool() { }
+
+        static NorthwindEntities _dbInstance;
+
+        public static NorthwindEntities DBInstance
+        {
+            get
+            {
+                if (_dbInstance == null) _dbInstance = new NorthwindEntities();
+                return _dbInstance;
+            }
+        }
+
+
+    }
+}
